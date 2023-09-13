e33a2cd5ff36842687473953649a596e25142961
monomooncodes <125437441+monomooncodes@users.noreply.github.com>
2023-09-13 10:53:01 +0000

    code

---
+++ 05_arraymethod.js
@@ -0,0 +1,20 @@
+// array method: it provied several build in methods that allows us to perform various operations on array. they are listed below:
+
+//push (): add  one or more elements to the end of an array and return the new length of the array
+ var p =[1,2,3,4,]
+p.push = [5,6]
+console.log(p);
+
+//pop() : remove the last element of the array and return the element 
+ var po = [1,2,3,4,5]
+ var removeArray = po.pop();
+ console.log(removeArray);
+ console.log(po);
+  
+ //shift(): remove first element of the array and return the element.
+  var shi = [1,2,3,4,5]
+  var shiftedEliment = shi.shift();
+  console.log(shi);
+
+  
+
