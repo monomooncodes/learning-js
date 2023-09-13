2aa3c81ed69332d4f8b87fbd8918b4c8648c5769
monomooncodes <125437441+monomooncodes@users.noreply.github.com>
2023-09-13 08:03:37 +0000

    code

---
+++ 04_datatype.js
@@ -0,0 +1,21 @@
+// in js there are several build-in-data types that you use to store and manipulate different kinds of values.
+// there are two  types ; premitive datatype and non- premitive data type
+//primitive datatype are: number, string, boolen,null, undefine,symbol
+let num =10;
+let name =" anna"
+let isActuve = true;
+let person = null;
+let data  ;
+let symbol = symbol();
+
+
+//non- primitive datatype: object, array, date
+// Object:g
+const human = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25")
+
